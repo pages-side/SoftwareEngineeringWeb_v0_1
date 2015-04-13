7bdea5bf8ebf4a840f1ab7b362891fdafd785912
pages-side <pages_side@yahoo.com>
2015-04-13 17:50:17 -0500

    Initial Revision

--- src/com/beans/CustomerBean.java
+++ src/com/beans/CustomerBean.java
@@ -3,23 +3,19 @@ package com.beans;
 import java.util.ArrayList;
 
 import com.business.Customer;
-import com.business.Game;
 
 public class CustomerBean {
-	ArrayList<Game> gameList = null;
+	ArrayList<Customer> customerList = null;
 	public CustomerBean(){
 		
 	}
-	public ArrayList<Game> getGameList() {
-		return gameList;
+	public ArrayList<Customer> getCustomerList() {
+		return customerList;
 	}
-	public void setGameList(ArrayList<Game> games) {
-		gameList = games;
+	public void setCustomerList(ArrayList<Customer> customerList) {
+		this.customerList = customerList;
 	}
-	public void setCustomerList(ArrayList<Customer> resultList) {
-		// TODO Auto-generated method stub
-		
-	}
+
 	
 	
 	
